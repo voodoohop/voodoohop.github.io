d80f7815445eb73a1dd51d0b7fe5741ccf1347e5
voodoohop <voodoo@voodoohop.com>
2019-04-17 06:33:53 +0200

    added test reason

---
+++ build/precache-manifest.e75eb21276edf795e0259f72074e7f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a83cc4b29cbd73208e5",
+    "url": "/static/css/main.b235c28f.chunk.css"
+  },
+  {
+    "revision": "7a83cc4b29cbd73208e5",
+    "url": "/static/js/main.7a83cc4b.chunk.js"
+  },
+  {
+    "revision": "b36f0c1c95563d3ae261",
+    "url": "/static/js/1.b36f0c1c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2342b05e19639d46eb7fb727b3ce8dd",
+    "url": "/static/media/logo.e2342b05.svg"
+  },
+  {
+    "revision": "72c03a907055a1906fea73d4f4ee95b1",
+    "url": "/index.html"
+  }
+];
